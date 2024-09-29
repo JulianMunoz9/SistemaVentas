1b49fa4245d46e75e673f0cf8715d6fd2db714d3
JulianMunoz9 <julian5270@outlook.com>
2024-09-28 23:36:29 -0500

    .

--- gestion_ventas/inventario/views.py
+++ gestion_ventas/inventario/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Cliente, Producto, Venta
+from .forms import ClienteForm, ProductoForm, VentaForm
+
+def lista_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'ventas/lista_productos.html', {'productos': productos})
+
+def crear_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_productos')
+    else:
+        form = ProductoForm()
+    return render(request, 'ventas/crear_producto.html', {'form': form})
